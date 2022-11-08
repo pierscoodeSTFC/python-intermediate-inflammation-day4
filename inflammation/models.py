bf0ea9f2dee4314aa3cb80022a95b277b6f2c6f5
pierscoodeSTFC <piers.coode@stfc.ac.uk>
2022-11-08 10:55:31 +0000

    Change to OOP

--- inflammation/models.py
+++ inflammation/models.py
@@ -9,6 +9,73 @@ and each column represents a single day across all patients.
 
 import numpy as np
 
+class Observation:
+    def __init__(self, day, value):
+        self.day = day
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Person:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+class Doctor(Person):
+    """Doctor and stuff
+    """
+    def __init__(self, name: str):
+        """_summary_
+        """
+        super().__init__(name)
+        self.patients = []
+    
+    def add_patient(self, new_patient):
+        # A crude check by name if this patient is already looked after
+        # by this doctor before adding them
+        for patient in self.patients:
+            if patient.name == new_patient.name:
+                return
+        self.patients.append(new_patient)
+
+
+class Patient(Person):
+    """Patient with a name and observations.
+    """
+    def __init__(self, name: str):
+        """_summary_
+        """
+        super().__init__(name)
+        self.observations = []
+
+    @property
+    def last_observation(self):
+        return self.observations[-1]
+
+    def add_observations(self, value, day=None):
+        """_summary_
+
+        Args:
+            value (_type_): _description_
+            day (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            _type_: _description_
+        """
+        if day is None:
+            if self.observations:
+                day = self.observations[-1].day + 1
+            else:
+                day = 0
+
+        new_observation = Observation(day, value)
+        self.observations.append(new_observation)
+        return new_observation
+
 
 def load_csv(filename):
     """Load a Numpy array from a CSV
